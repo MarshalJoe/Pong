141de8e882e451cc2fcebf0f296fadffde14aec7
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-04-17 15:20:08 -0500

    Update and draw functions added

--- game.js
+++ game.js
@@ -32,10 +32,23 @@ Game.prototype.start = function () {
 	var self = this,
 				fps = 60,
 				interval = 1000 / fps; //ms per frame
-	
+
 	setInterval(function() {
 
 		self.update();
 		self.draw();
 	}, interval)
+}
+
+Game.prototype.update = function () {
+	this.entities.forEach(function(entity) {
+		if (entity.update) entity.update()
+	})
+}
+
+
+Game.prototype.update = function () {
+	this.entities.forEach(function(entity) {
+		if (entity.update) entity.update()
+	})
 }
