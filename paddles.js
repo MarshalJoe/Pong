a0c45915a586bb59eeffb2934c39dbe1e347cd01
MarshalJoe <joseph.charles.marshall@gmail.com>
2014-04-17 18:26:19 -0500

    Adds barrier effect to paddles

--- paddles.js
+++ paddles.js
@@ -44,3 +44,27 @@ Player.prototype.update = function () {
 
 	Paddle.prototype.update.apply(this, arguments)
 }
+
+
+function Bot () {
+	Paddle.call(this)
+
+	this.x = game.width - this.width - 20
+}
+
+Bot.prototype = Object.create(Paddle.prototype)
+Bot.prototype.constructor = Bot
+
+Bot.prototype.update = function () {
+	var speed = 5
+
+	if (this.y < game.ball.y) {
+		this.yVelocity = speed
+	} else if (this.y > game.ball.y) {
+		this.yVelocity = -speed
+	}
+
+
+	Paddle.prototype.update.apply(this, arguments)
+
+}
